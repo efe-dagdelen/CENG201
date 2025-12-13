f5ec3c928dbc191eaa1e3535bd91ecd9ed6286a4
efe-dagdelen <efe_dagdelen@outlook.com>
2025-12-13 18:47:04 +0300

    update

--- src/TimeComplexity/TimeComplexityExamples.java
+++ src/TimeComplexity/TimeComplexityExamples.java
@@ -1,4 +1,16 @@
 package TimeComplexity;
 
 public class TimeComplexityExamples {
+
+    // O(1) - Constant Time
+    public static void constantTime(int[] arr) {
+        System.out.println("O(1) Example:");
+        if (arr.length > 0)
+            System.out.println("First element: " + arr[0]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        constantTime(arr);
+    }
 }
