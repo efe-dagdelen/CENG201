213a22bf79775cd1128d963a56bfbb3d18158df7
efe-dagdelen <efe_dagdelen@outlook.com>
2025-12-15 18:11:25 +0300

    tree

---
+++ src/Tree/Node.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
